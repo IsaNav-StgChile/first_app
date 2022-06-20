d9d04296fc7008e0d21c448f83855957a495603e
Isabel N <isanpe@gmail.com>
2022-06-20 01:22:35 -0400

    Tercer commit agrega pagina three

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
   def two
   end
 
+  def two
+  end
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
-
+  get 'pages/three'
+  
   root 'pages#one'
 
 end
